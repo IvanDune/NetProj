dcdce4a3d8d1222451d14703fdc3763a889a261d
SyLeyaN <Divanalex@gmail.com>
2023-02-21 13:16:20 +0400

    Find a way to deploy project into server

---
+++ target/classes/db/migration/V2__Add_admin.sql
@@ -0,0 +1,3 @@
+--insert into "user" (id,login, password, email, nickname, active) values (1, 'Admin', 'Admin', 'Admin', 'Admin', true );
+--insert into user_role (user_id, roles) values (1, 'USER'), (1, 'ADMIN');
+
