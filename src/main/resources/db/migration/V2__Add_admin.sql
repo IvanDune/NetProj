b6b1a886bf4456ea71d98822205b9f2cc074615e
SyLeyaN <Divanalex@gmail.com>
2023-08-23 10:17:28 +0400

    Delete db migration to use liquibase spring boot

--- src/main/resources/db/migration/V2__Add_admin.sql
+++
@@ -1,3 +0,0 @@
---insert into "user" (id,login, password, email, nickname, active) values (1, 'Admin', 'Admin', 'Admin', 'Admin', true );
---insert into user_role (user_id, roles) values (1, 'USER'), (1, 'ADMIN');
-
